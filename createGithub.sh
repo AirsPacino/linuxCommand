59b2e8b66fe17d40edbd8926080ee41f4050f0ab
Airs_Pacino <airs_pacino@sina.com>
2020-12-21 21:17:45 +0800

    first push

---
+++ createGithub.sh
@@ -0,0 +1,5 @@
+mkdir test
+cd test
+git init
+# create a repository in your github
+git remote add origin git@github.com:AirsPacino/linuxCommand.git
---
+++ learnLinux.sh
@@ -0,0 +1,4 @@
+# 查看系统每个进程启动时间
+systemd-analyze blame
+# 配置无线网络及热点
+nm-connection-editor
